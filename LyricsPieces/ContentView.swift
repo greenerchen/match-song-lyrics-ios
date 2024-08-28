589c9ea9d53f44bad65b7f1476859e8c3cf5089c
Greener Chen <greener@gmail.com>
2024-08-28 16:55:42 +0800

    Initial Commit

---
+++ LyricsPieces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LyricsPieces
+//
+//  Created by Greener Chen on 2024/8/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LyricsPieces/LyricsPiecesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsPiecesApp.swift
+//  LyricsPieces
+//
+//  Created by Greener Chen on 2024/8/28.
+//
+
+import SwiftUI
+
+@main
+struct LyricsPiecesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
