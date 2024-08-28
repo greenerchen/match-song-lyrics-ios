18d233aab8aa94572da0963a8015678bcf876a0b
Greener Chen <greener@gmail.com>
2024-08-28 17:55:01 +0800

    Add ShazamStartView

---
+++ LyricsPieces/Views/ShazamStartView.swift
@@ -0,0 +1,18 @@
+//
+//  ShazamStartView.swift
+//  LyricsPieces
+//
+//  Created by Greener Chen on 2024/8/28.
+//
+
+import SwiftUI
+
+struct ShazamStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShazamStartView()
+}
