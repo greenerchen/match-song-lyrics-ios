c02f18faf134b78db3f0f570dc46c4db7fa8fee7
Greener Chen <greener@gmail.com>
2024-09-05 10:55:05 +0800

    Update ShazamResultView

--- LyricsPieces/Views/ShazamResultView.swift
+++ LyricsPieces/Views/ShazamResultView.swift
@@ -14,7 +14,6 @@ struct ShazamResultView: View {
     @Environment(\.openURL) var openURL
     
     @State private var isPresented: Bool = false
-    @State private var hasLyrics: Bool = false
     @State private var lyricsURL: URL?
     @State private var cancellable = Set<AnyCancellable>()
     
@@ -24,11 +23,14 @@ struct ShazamResultView: View {
         VStack {
             if let match = result.match,
                let item = match.mediaItems.first {
-                VStack {
+                ZStack(alignment: .bottomLeading) {
                     AsyncImage(url: item.artworkURL) { image in
                         image.resizable()
+                            .aspectRatio(contentMode: .fit)
                     } placeholder: {
-                        Image("songwork")
+                        Image("songwork").resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 40 - 34)
                     }
                     .onTapGesture(perform: {
                         guard let shazamWebURL = item.webURL else {
@@ -36,46 +38,70 @@ struct ShazamResultView: View {
                         }
                         openURL(shazamWebURL)
                     })
-                    .aspectRatio(1, contentMode: .fit)
-                    Text(item.title ?? "")
-                        .lineLimit(3)
-                    Text("by \(item.artist ?? "")")
-                    Text("matched at \(item.matchTime)")
-                    HStack {
-                        Button(action: {
-                            fetchLyrics()
-                        }, label: {
-                            Label("Read Lyrics", systemImage: "music.note.list")
-                        })
-                        .sheet(isPresented: $isPresented,
-                               content: {
-                            if let lyricsURL = lyricsURL {
-                                WebView(url: lyricsURL)
-                                    .presentationDetents([.medium, .large])
-                            }
-                            if !hasLyrics {
-                                Text("Lyrics Not Found")
-                                    .presentationDetents([.medium, .large])
-                            }
-                        })
-
-                        Image("apple.music.badge")
-                            .resizable()
-                            .frame(width: 136, height: 40)
-                            .onTapGesture {
-                                guard let appleMusicURL = item.appleMusicURL else {
-                                    return
-                                }
-                                openURL(appleMusicURL)
-                            }
+                    
+                    VStack(alignment: .leading) {
+                        Text(item.title ?? "")
+                            .lineLimit(3)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text("\(item.artist ?? "")")
+                            .lineLimit(3)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("Matched at \(item.matchTime)")
+                            .font(.subheadline)
                     }
+                    .containerRelativeFrame(.horizontal){ size, axis in
+                        // FIXME: when the song title is long, size.significand is too big for this case
+                        size * size.significand
+                    }
+                    .background(
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [.clear, .themeBackground]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
                 }
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.top)
+                
+                HStack {
+                    Button(action: {
+                        fetchLyrics()
+                    }, label: {
+                        Label("Read Lyrics", systemImage: "music.note.list")
+                            .frame(height: 40)
+                            .clipShape(Capsule())
+                    })
+                    .sheet(isPresented: $isPresented,
+                           content: {
+                        if let lyricsURL = lyricsURL {
+                            WebView(url: lyricsURL)
+                                .presentationDetents([.medium, .large])
+                        } else {
+                            Text("Lyrics Not Found")
+                                .presentationDetents([.medium, .large])
+                        }
+                    })
+                    
+                    Image("apple.music.badge")
+                        .resizable()
+                        .frame(width: 136, height: 40)
+                        .onTapGesture {
+                            guard let appleMusicURL = item.appleMusicURL else {
+                                return
+                            }
+                            openURL(appleMusicURL)
+                        }
+                }
+                Spacer()
+//                Label("Powered by Shazam", systemImage: "shazam.logo")
             } else {
                 Text("Uh oh. Nothing found.")
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.themeBackground)
     }
     
     private func fetchLyrics() {
@@ -86,12 +112,6 @@ struct ShazamResultView: View {
                 artistNames: item.artist ?? ""
             )
             .sink { completion in
-                switch completion {
-                case .finished:
-                    hasLyrics = true
-                case .failure:
-                    hasLyrics = false
-                }
                 isPresented.toggle()
             } receiveValue: { song in
                 lyricsURL = song.url
