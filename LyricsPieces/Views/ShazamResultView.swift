0dc54e9294a5557ad6932a106f1baac038b7324c
Greener Chen <greener@gmail.com>
2024-09-12 11:32:11 +0800

    Replace GeniusAPI with MusixmatchAPI

--- LyricsPieces/Views/ShazamResultView.swift
+++ LyricsPieces/Views/ShazamResultView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 import ShazamKit
 import Combine
-import GeniusLyricsAPI
+import MusixmatchAPI
 import SDWebImageSwiftUI
 
 struct ShazamResultView: View {
     @Environment(\.openURL) var openURL
     
     @State private var isPresented: Bool = false
-    @State private var lyricsURL: URL?
+    @State private var lyricsViewModel: LyricsViewModel?
     @State private var cancellable = Set<AnyCancellable>()
     
     var result: ShazamMatchResult
@@ -62,7 +62,9 @@ struct ShazamResultView: View {
                 Spacer(minLength: 10)
                 HStack {
                     Button(action: {
-                        fetchLyrics()
+                        Task {
+                            await fetchLyrics()
+                        }
                     }, label: {
                         Label("Read Lyrics", systemImage: "music.note.list")
                     })
@@ -70,8 +72,8 @@ struct ShazamResultView: View {
                     .buttonStyle(.borderedProminent)
                     .sheet(isPresented: $isPresented,
                            content: {
-                        if let lyricsURL = lyricsURL {
-                            WebView(url: lyricsURL)
+                        if let vm = lyricsViewModel, vm.hasLyrics {
+                            WebView(url: nil, htmlString: vm.makeHtmlString())
                                 .presentationDetents([.medium, .large])
                         } else {
                             Text("Lyrics Not Found")
@@ -96,20 +98,27 @@ struct ShazamResultView: View {
         }
     }
     
-    private func fetchLyrics() {
+    private func fetchLyrics() async {
+        let client = MusixmatchAPIClient()
         if let match = result.match,
-           let item = match.mediaItems.first {
-            GeniusLyricsClient().search(
-                title: item.title ?? "",
-                artistNames: item.artist ?? ""
-            )
-            .sink { completion in
-                isPresented.toggle()
-            } receiveValue: { song in
-                lyricsURL = song.url
+           let item = match.mediaItems.first,
+           let tracklist = try? await client.searchTrack(item.title ?? "", artist: item.artist ?? ""),
+           let track = tracklist.first {
+            lyricsViewModel = LyricsViewModel(
+                trackName: track.trackName,
+                artistName: track.artistName,
+                hasLyrics: track.hasLyrics,
+                lyricsBody: track.lyricsBody,
+                lyricsCopyright: track.lyricsCopyright, backlinkUrl: track.backlineUrl, scriptTrackingUrl: nil)
+            if track.hasLyrics,
+               let lyrics = try? await client.getLyrics(trackId: track.id) {
+                lyricsViewModel?.lyricsBody = lyrics.body
+                lyricsViewModel?.lyricsCopyright = lyrics.copyright
+                lyricsViewModel?.scriptTrackingUrl = lyrics.scriptTrackingUrl
+                
             }
-            .store(in: &cancellable)
         }
+        isPresented.toggle()
     }
 }
 
@@ -118,3 +127,4 @@ struct ShazamResultView: View {
     ShazamResultView(result: ShazamMatchResult(match: nil))
 //    ShazamResultView(result: ShazamMatchResult(match: matchStub))
 }
+
