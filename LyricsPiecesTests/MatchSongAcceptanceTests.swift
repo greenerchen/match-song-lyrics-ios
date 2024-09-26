9512792d876a4e27d52d03efe37c4ceebb44f45f
Greener Chen <greener@gmail.com>
2024-09-26 18:39:40 +0800

    Add match song acceptance tests

---
+++ LyricsPiecesTests/MatchSongAcceptanceTests.swift
@@ -0,0 +1,23 @@
+//
+//  MatchSongAcceptanceTests.swift
+//  LyricsPiecesTests
+//
+//  Created by Greener Chen on 2024/9/26.
+//
+
+import XCTest
+
+final class MatchSongAcceptanceTests: XCTestCase {
+
+    func test_shazam_whenAUserHasConnectivityAndASongRecording_gotSongInfoAndReadLyricsActions() throws {
+        
+    }
+
+    func test_shazam_whenAUserHasNoConnectivityAndASongRecording_gotNoConnectivityError() throws {
+        
+    }
+    
+    func test_shazam_whenAUserHasConnectivityButNoSongMatched_gotNoSongMatchedError() throws {
+        
+    }
+}
