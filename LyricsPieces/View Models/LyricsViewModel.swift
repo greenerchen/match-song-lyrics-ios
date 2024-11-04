77f982edf671db03b6f14cc18f7af9ee9d762b00
Greener Chen <greener@gmail.com>
2024-11-04 16:04:59 +0800

    Change the font used in web view to a built-in font

--- LyricsPieces/View Models/LyricsViewModel.swift
+++ LyricsPieces/View Models/LyricsViewModel.swift
@@ -142,7 +142,7 @@ private let lyricsHtmlTemplate: String = """
 <style>
 body {font-family: "Times New Roman", Georgia, Serif;}
 h1, h2, h3, h4, h5, h6 {
-  font-family: "Playfair Display";
+  font-family: "Serif";
   letter-spacing: 5px;
 }
 </style>
@@ -205,7 +205,7 @@ private let errorHtmlTemplate: String = """
 <style>
 body {font-family: "Times New Roman", Georgia, Serif;}
 h1, h2, h3, h4, h5, h6 {
-  font-family: "Playfair Display";
+  font-family: "Serif";
   letter-spacing: 5px;
 }
 </style>
